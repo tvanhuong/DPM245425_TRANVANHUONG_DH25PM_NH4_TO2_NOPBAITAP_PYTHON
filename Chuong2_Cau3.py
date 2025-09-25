2899876db2b718e190483bd6cc6137285a2b7008
tvanhuong <ht6883392@gmail.com>
2025-09-25 10:00:29 +0700

    Nop bt cau 3

--- Chuong2_Cau3.py
+++ Chuong2_Cau3.py
@@ -3,12 +3,19 @@
 Viết chương trình nhập vào điểm ba môn Toán, Lý, Hóa của một học sinh. In ra
 điểm trung bình của học sinh đó với hai số lẻ thập phân.'''
 import math
-print("Chương trình tính điểm trung bình")
-toan,ly,hoa=eval(input("Nhập điểm toán,lý,hóa:"))
-print("Điểm toán=",toan)
-print("Điểm lý=",ly)
-print("Điểm hóa=",hoa)
+toan=float(input("Nhập điểm Toán:"))
+ly=float(input("Nhập điểm lý:"))
+hoa=float(input("Nhập điểm hóa:"))
+while (toan >= 10 or ly >= 10 or hoa >= 10):
+        print("Điểm nhập vào phải nhỏ hơn hoặc bằng 10. Vui lòng nhập lại.\n")
+        toan=float(input("Nhập điểm Toán:"))
+        ly=float(input("Nhập điểm lý:"))
+        hoa=float(input("Nhập điểm hóa:"))
+        
 dtb=(toan+ly+hoa)/3
 print("Điểm trung bình=",dtb)
-print("Điểm làm tròn=",round(dtb,2))
+print("Điểm lam tron=",round(dtb,2))
+           
+     
+
 
